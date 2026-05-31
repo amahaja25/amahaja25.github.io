c77784ba7950a4f6f70947a6f70180d450da3b5d
Apurva Mahajan <apurvamahajan925@gmail.com>
2026-05-31 01:31:19 -0400

    fixed some stuff on mobile, added meta tags in head, increased margin bottom and added favicon.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<title>Apurva Mahajan</title>
+	<meta name="description" content="This is where the description goes for SEO" />
+	<meta name="author" content="Apurva Mahajan" />
+	<meta name="google-site-verification" content="UNejqFTyS3ARTaeQwIdanmhfEBPh63HP0-IHhJFN2bQ" />
+	<meta name="description" content="Apurva Mahajan" />
+	<meta
+		name="keywords"
+		content="Apurva Mahajan, journalist, journalism, data, developer, texas, texas tribune, washington post, projects, portfolio, website, UMD, University of Maryland"
+	/>
+</svelte:head>
+
 {@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,14 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-      adapter: adapter({
-        fallback: '404.html'
-      }),
-      paths: {
-        base: '' 
-      }
-    }
-  };
-  
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: ''
+		}
+	}
+};
+
 export default config;
